5d95464e28087c6b55ae02bceb03293140a9fe2b
lhermoso <leonardo@elif.capital>
2023-04-23 20:14:11 -0400

    Docker

--- strategies/cron_scripts/volatility.sh
+++ strategies/cron_scripts/volatility.sh
@@ -2,5 +2,5 @@
 pid=$(ps xa | pgrep -f manage.py\ start_vol)
 if [[ -z "$pid" ]]; then
   echo "Starting Volatility Trading"
-  /home/leonardo/django-trader/venv/bin/python /home/leonardo/django-trader/manage.py run_vol
+  /home/leonardo/django-trader/venv/bin/python /home/leonardo/django-trader/manage.py start_vol
 fi
